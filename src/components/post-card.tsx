901743f58ecb11edcfd7837516108a94cb7341a3
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-08-14 11:25:03 +0900

    add sns icons in footer

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from '../styles/postCard.module.scss'
+import styles from '../styles/post-card.module.scss'
 
 const PostCard = ({
   title,
---
+++ src/components/sns-icons.tsx
@@ -0,0 +1,32 @@
+import styles from '../styles/sns-icons.module.scss'
+
+interface SnsItem {
+  name: string,
+  url: string
+}
+
+const SnsIcons = () => {
+  const snsItems: SnsItem[] = [
+    {
+      name: 'twitter',
+      url: 'https://twitter.com/HelloJapanese1'
+    },
+    {
+      name: 'instagram',
+      url: 'https://www.instagram.com/hellojapanese1'
+    },
+  ]
+  const path: string = 'https://hello-japanese.s3.ap-northeast-1.amazonaws.com/2020/04/'
+
+  return (
+    <div className={styles.sns}>
+      {snsItems.map(({name, url}) => (
+        <a href={url}>
+          <img src={`${path}${name}.png`} alt={name} width={50} />
+        </a>
+      ))}
+    </div>
+  )
+}
+
+export default SnsIcons
