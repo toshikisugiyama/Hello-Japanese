eaa00bb56a447d843f6f5123acebb2f04c2c3d27
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-08-14 12:24:11 +0900

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="UTF-8" key="charset" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" key="viewport" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="/android-chrome-512x512.png" />
       </Head>
       <Component {...pageProps} />
     </>
